d8cda3c844c8a17a21620d9057a0a3e141e93e34
Sai Siddarth <siddarthsiddu086@gmail.com>
2024-04-08 17:38:30 +0530

    installed unicons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   return (
     <div className="App">
       <h1 className='text-2xl'>Hello world</h1>
+      <UilReact/>
     </div>
   );
 }
